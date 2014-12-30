robromano/cilantro

commit 26e3689a516a8d9c340df3cfd07c8a9d5ee9fa67

Add stats models for query and context models
This commit also makes the required changes in the context panel to account for the count being on the stats now.

Signed-off-by: Don Naegely <naegelyd@gmail.com>

src/js/cilantro/models/context.js
@@ -35,8 +35,7 @@ define([
      * The context model provides an interface for [un]applying filters. Filters
      * are uniquely identified by field, concept, or both.
      */
-    var Context = base.Model.extend({
-
+    var Context = base.StatsSupportedModel.extend({
         options: {
             saveDelay: 300
         },
@@ -92,21 +91,24 @@ define([
                 }, this);
             });
 
-            // Trigger Cilantro event when context is saved
-            this.on('sync', function(model, attrs, options) {
-                options = options || {};
-
-                // Validate the context on sync
-                this.validate();
-
-                if (options.silent !== true) {
-                    c.trigger(c.CONTEXT_SYNCED, this, 'success');
-                }
-            });
-
             // Define a debounced save method for handling rapid successions
             // of [un]apply events.
             this._save = _.debounce(this.save, this.options.saveDelay);
+        },
+
+        onSync: function(model, resp, options) {
+            base.StatsSupportedModel.prototype.onSync.call(
+                this, model, resp, options);
+
+            // Trigger Cilantro event when context is saved
+            options = options || {};
+
+            // Validate the context on sync
+            this.validate();
+
+            if (options.silent !== true) {
+                c.trigger(c.CONTEXT_SYNCED, this, 'success');
+            }
         },
 
         validate: function() {

src/js/cilantro/models/base.js
@@ -3,8 +3,9 @@
 define([
     'underscore',
     'backbone',
+    'loglevel',
     '../utils'
-], function(_, Backbone, utils) {
+], function(_, Backbone, loglevel, utils) {
 
     // Base model for Cilantro.
     var Model = Backbone.Model.extend({
@@ -13,6 +14,22 @@ define([
             this.links = {};
 
             Backbone.Model.prototype.constructor.call(this, attrs, options);
+
+            // TODO: Should this be limited to listening to those attributes
+            // that are in link templates on the collection?
+            if (this.collection) {
+                this.on('change', function() {
+                    // TODO: Common method for this since it's similar to what is
+                    // in parse()?
+                    if (this.collection) {
+                        _.extend(
+                            this.links,
+                            utils.getLinksFromTemplates(
+                                this, this.collection.linkTemplates)
+                        );
+                    }
+                }, this);
+            }
         },
 
         url: function() {
@@ -53,6 +70,61 @@ define([
         }
     });
 
+    var StatModel = Model.extend({
+        constructor: function(attrs, options) {
+            Model.prototype.constructor.call(this, attrs, options);
+
+            this.parent = {};
+
+            if (!(this.parent = attrs.parent)) {
+                throw new Error('parent model required');
+            }
+
+            this.listenTo(this.parent, 'request', this.onParentRequest);
+            this.listenTo(this.parent, 'sync', this.onParentSync);
+
+            if (this.parent.collection) {
+                this.listenTo(this.parent.collection, 'reset', this.onParentReset);
+            }
+        },
+
+        onParentRequest: function() {},
+
+        onParentReset: function() {
+            this.fetch();
+        },
+
+        onParentSync: function() {
+            this.fetch();
+        },
+
+        url: function() {
+            if (!this.parent.id && this.parent.collection) {
+                return this.parent.collection.links.self.uri;
+            }
+            else if (this.parent.links.stats && this.parent.links.stats.uri) {
+                return this.parent.links.stats.uri;
+            }
+            else {
+                throw new Error('Stat supported model has no stats URL defined.');
+            }
+        }
+    });
+
+    var StatsSupportedModel = Model.extend({
+        statModel: StatModel,
+
+        constructor: function(attrs, options) {
+            if (!this.statModel) {
+                throw new Error('statModel must be defined');
+            }
+
+            Model.prototype.constructor.call(this, attrs, options);
+
+            this.stats = new this.statModel({parent: this});
+        }
+    });
+
     var Collection = Backbone.Collection.extend({
         model: Model,
 
@@ -61,19 +133,6 @@ define([
             this.links = {};
 
             Backbone.Collection.prototype.constructor.call(this, attrs, options);
-
-            this.on('reset', this._updateModelLinks, this);
-        },
-
-        _updateModelLinks: function() {
-            var templates = this.linkTemplates;
-
-            _.each(this.models, function(model) {
-                _.extend(
-                    model.links,
-                    utils.getLinksFromTemplates(model, templates)
-                );
-            });
         },
 
         _parseLinks: function(collection, xhr) {
@@ -161,6 +220,8 @@ define([
 
     return {
         Model: Model,
+        StatModel: StatModel,
+        StatsSupportedModel: StatsSupportedModel,
         Collection: Collection,
         SessionCollection: SessionCollection
     };

src/js/cilantro/models/query.js
@@ -7,8 +7,11 @@ define([
     './view'
 ], function(base, c, context, view) {
 
+    var QueryStats = base.Model.extend({});
 
-    var Query = base.Model.extend({
+    var Query = base.StatsSupportedModel.extend({
+        statModel: QueryStats,
+
         constructor: function(attrs, options) {
             attrs = attrs || {};
 

src/js/cilantro/ui/context/actions.js
@@ -23,8 +23,6 @@ define([
         },
 
         modelEvents: {
-            'change:count': 'render',
-            'sync': 'showCount',
             'request': 'showLoad'
         },
 
@@ -32,10 +30,17 @@ define([
             'add remove reset': 'renderRemoveAll'
         },
 
+        initialize: function() {
+            this.model.stats.on('sync', this.showCount, this);
+        },
+
         serializeData: function() {
             var attrs = _.clone(this.model.attributes);
+
+            attrs.count = this.model.stats.get('count');
             attrs.prettyCount = c.utils.prettyNumber(
                 attrs.count, c.config.get('threshold'));
+
             return attrs;
         },
 
@@ -57,6 +62,7 @@ define([
             this.ui.count.show();
             this.ui.units.show();
             this.ui.loading.hide();
+            this.render();
         },
 
         renderRemoveAll: function() {

commit 8a06d9da1e1657d63c3ec3292b91ec105176d056

Rebind abstracted sync handler
This bug was introduced in 26e3689.

Signed-off-by: Byron Ruth <b@devel.io>

src/js/cilantro/models/context.js
@@ -91,15 +91,14 @@ define([
                 }, this);
             });
 
+            this.on('sync', this.onSync);
+
             // Define a debounced save method for handling rapid successions
             // of [un]apply events.
             this._save = _.debounce(this.save, this.options.saveDelay);
         },
 
         onSync: function(model, resp, options) {
-            base.StatsSupportedModel.prototype.onSync.call(
-                this, model, resp, options);
-
             // Trigger Cilantro event when context is saved
             options = options || {};
 